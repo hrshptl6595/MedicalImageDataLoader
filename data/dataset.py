a3d8e92686a2fcb6b8c69cc2ad1fd5ad9c3793b4
Harsh Patel <harshrocks2@live.com>
2017-03-20 20:31:18 +0530

    Initial Commit
    Added Data classes

---
+++ data/dataset.py
@@ -0,0 +1,35 @@
+class Dataset(object):
+	"""An abstract class representing a Dataset.
+
+	All other datasets should subclass it. All subclasses shoudl override
+	``_len__``, that provides the size of the dataset, and ``__getitem__``,
+	supporting integer indexing in range from 0 to len(self) executive.
+	"""
+
+	def __getitem__(self, index):
+		raise NotImplementedError
+
+	def __len__(self):
+		raise NotImplementedError
+
+class TensorDataset(Dataset):
+	"""Dataset wrapping data and target tensors.
+
+	Each sample will be retrieved by indexing both tensors along the first
+	dimensions.
+
+	Arguments:
+		data_tensor (Tensor): contains sample data.
+		target_tensor (Tensor): contains sample targets (labels).
+	"""
+
+	def __init__(self, data_tensor, target_tensor):
+		assert data_tensor.size(0) == target_tensor.size(0)
+		self.data_tensor = data_tensor
+		self.target_tensor = target_tensor
+
+	def __getitem__(self, index):
+		return self.data_tensor[index], self.target_tensor[index]
+
+	def __len__(self):
+		return self.data_tensor.size(0)
